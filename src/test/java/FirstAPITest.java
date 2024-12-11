b6263bce764286c803ed1f5e2b48e373631c6b0f
Kseniya298 <kseniya.ivanova2016@gmail.com>
2024-12-11 16:27:55 +0300

    FirstAPITest

---
+++ src/test/java/FirstAPITest.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class FirstAPITest {
+    @Test
+    public void testAPI(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
