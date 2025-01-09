93dbdea7abfc30999ad92ebce4c5131a4dafea7d
Kseniya298 <kseniya.ivanova2016@gmail.com>
2025-01-09 14:37:30 +0300

    Exercise5

--- src/test/java/ParserJSON.java
+++ src/test/java/ParserJSON.java
@@ -8,12 +8,7 @@ import java.util.Map;
 public class ParserJSON {
     @Test
     public void testRestAssured(){
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "John");
-
         JsonPath response = RestAssured
-                .given()
-                .queryParams(params)
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
         Object answer = response.get("messages");
